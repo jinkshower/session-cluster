e21bdd4d90212ef6a3a6bc0b1cf12446791450b2
jinkshower <askthem@naver.com>
2024-07-15 13:36:16 +0900

    initial commit

---
+++ src/main/java/hiyen/sessioncluster/SessionClusterApplication.java
@@ -0,0 +1,13 @@
+package hiyen.sessioncluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionClusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionClusterApplication.class, args);
+	}
+
+}
