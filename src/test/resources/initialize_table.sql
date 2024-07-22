88ba244f149e1fd4cfbc8747afd7eb033710d626
jinkshower <askthem@naver.com>
2024-07-22 15:04:41 +0900

    feat: embedded redis를 이용해 테스트에서 실 redis 분리

---
+++ src/test/resources/initialize_table.sql
@@ -0,0 +1,8 @@
+drop table if exists member;
+create table member (
+                        id bigint not null auto_increment,
+                        email varchar(255) not null,
+                        password varchar(255) not null,
+                        name varchar(255) not null,
+                        primary key (id)
+);
